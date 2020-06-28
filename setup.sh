01531ec2ef61b2581656f6c3f384dc87d13cce74
John Sammons <john_sammons@hotmail.com>
2020-06-28 18:48:20 -0400

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+localectl set-x11-keymap us chromebook
+rm galliumos.gpg*
+wget https://apt.galliumos.org/galliumos.gpg
+sudo apt-key add galliumos.gpg
+sudo cp galliumos.list /etc/apt/sources.list.d/.
+sudo apt update
+sudo apt upgrade
+sudo apt install galliumos-sandy
+sudo update-grub
+sudo update-initramfs -u -k all
+sudo apt install grub-customizer
+file=/usr/bin/kblight
+[ -e "$file" ] && sudo rm "$file"
+sudo ln -s $(pwd)/kblightdb /usr/bin/kblight 
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < kbdsettings
 cp .xprofile ~/.
