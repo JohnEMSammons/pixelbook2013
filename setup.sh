d7ed20b33624ddc4366bdbce351bf8fc20b7c51c
John E. M. Sammons <john_sammons@hotmail.com>
2020-06-29 21:32:23 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ sudo update-initramfs -u -k all
 sudo apt install grub-customizer
 file=/usr/bin/kblight
 [ -e "$file" ] && sudo rm "$file"
-sudo ln -s $(pwd)/kblightdb /usr/bin/kblight 
+sudo ln -s $(PWD)/kblightdb /usr/bin/kblight 
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < kbdsettings
 cp .xprofile ~/.
