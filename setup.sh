a967616002b44883a126e99218930773a6fbaeeb
John Sammons <john_sammons@hotmail.com>
2020-07-02 01:56:07 -0400

    added mainline download of kernel

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 localectl set-x11-keymap us chromebook
 sudo apt update
 sudo apt upgrade
+./ubuntu-mainline-kernel.sh -i 5.7.7 -do -p .
 sudo apt install -y ./*.deb
 sudo apt install grub-customizer
 sudo rm -f /usr/bin/kblight
 sudo ln -s $PWD/kblightdb /usr/bin/kblight
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < kbdsettings
+./importkeys.sh
 cp .xprofile ~/.
