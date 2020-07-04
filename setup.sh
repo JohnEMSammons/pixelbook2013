496ba589d360d9d437caff2ef2faccaa5f09c5e0
John Sammons <john_sammons@hotmail.com>
2020-07-04 09:20:53 -0400

    added caniberra packages for java

--- setup.sh
+++ setup.sh
@@ -4,8 +4,15 @@ sudo apt update
 sudo apt upgrade
 ./ubuntu-mainline-kernel.sh -i 5.7.7 -do -p .
 sudo apt install -y ./*.deb
-sudo apt install grub-customizer
+sudo apt install -y grub-customizer
+sudo apt install -y build-essential git dkms
+git clone https://github.com/brektrou/rtl8821CU.git
+cd rtl8821CU
+chmod +x dkms-install.sh
+sudo ./dkms-install.sh
+sudo modprobe 8821cu
 sudo rm -f /usr/bin/kblight
 sudo ln -s $PWD/kblightdb /usr/bin/kblight
 ./importkeys.sh
 cp .xprofile ~/.
+sudo apt install -y libcanberra-gtk-module libcanberra-gtk3-module
